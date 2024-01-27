ff13d77b757cb2b37e7ddb514c1d9c1ddb808244
Davide Garberi <dade.garberi@gmail.com>
2024-01-27 12:16:10 +0100

    src: main: clients: Implement AddressEncode test

--- src/main/java/clients/AddressEncode.java
+++ src/main/java/clients/AddressEncode.java
@@ -1,17 +1,20 @@
 package clients;
 
-/** AddressEncode */
+import java.util.Scanner;
+import mua.Address;
+
 public class AddressEncode {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-  /**
-   * Tests address encoding
-   *
-   * <p>Reads three lines from stdin corresponding to the (possibly empty) <em>display name</em>,
-   * <em>local</em>, and <em>domain</em> parts of the address and emits a line in the stout
-   * containing the encoding of the email address.
-   *
-   * @param args not used.
-   */
-  // public static void main(String[] args) {}
+    String displayName = scanner.nextLine();
+    String local = scanner.nextLine();
+    String domain = scanner.nextLine();
 
+    Address address = new Address(displayName, local, domain);
+
+    System.out.println(address);
+
+    scanner.close();
+  }
 }
