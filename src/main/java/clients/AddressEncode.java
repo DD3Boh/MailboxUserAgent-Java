b415e4dde52f800b86dcfa611ea90641cf2bb0eb
Davide Garberi <dade.garberi@gmail.com>
2024-01-27 17:36:30 +0100

    !fixup: add back addressencode comment

--- src/main/java/clients/AddressEncode.java
+++ src/main/java/clients/AddressEncode.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 import mua.Address;
 
 public class AddressEncode {
+
+  /**
+   * Tests address encoding
+   *
+   * <p>Reads three lines from stdin corresponding to the (possibly empty) <em>display name</em>,
+   * <em>local</em>, and <em>domain</em> parts of the address and emits a line in the stout
+   * containing the encoding of the email address.
+   *
+   * @param args not used.
+   */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
