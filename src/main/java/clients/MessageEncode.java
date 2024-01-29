674c987c5bbf4ab87d5b7945ce999c49118190c8
Davide Garberi <dade.garberi@gmail.com>
2024-01-29 02:39:08 +0100

    Implement MessageEncode test

--- src/main/java/clients/MessageEncode.java
+++ src/main/java/clients/MessageEncode.java
@@ -1,7 +1,13 @@
 package clients;
 
+import mua.*;
+import utils.ASCIICharSequence;
+
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 /** MessageEncode */
 public class MessageEncode {
@@ -28,6 +34,73 @@ public class MessageEncode {
    *
    * @param args not used
    */
-  // public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String displayName, local, domain;
+    List<Header<?>> headers = new ArrayList<>();
+    List<MessagePart> parts = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+
+    displayName = scanner.nextLine();
+    local = scanner.nextLine();
+    domain = scanner.nextLine();
+
+    headers.add(new SenderHeader(new Address(displayName, local, domain)));
+
+    displayName = scanner.nextLine();
+    local = scanner.nextLine();
+    domain = scanner.nextLine();
+
+    Recipients recipients = new Recipients();
+    recipients.addAddress(new Address(displayName, local, domain));
+
+    displayName = scanner.nextLine();
+    local = scanner.nextLine();
+    domain = scanner.nextLine();
+    recipients.addAddress(new Address(displayName, local, domain));
+
+    headers.add(new RecipientsHeader(recipients));
+
+    String subject = scanner.nextLine();
+    headers.add(new SubjectHeader(new Subject(subject)));
+
+    headers.add(new DateHeader(new Date(DATE)));
+
+    String text = scanner.nextLine();
+    String html = scanner.nextLine();
+    scanner.close();
+
+    if (!text.isEmpty() && !html.isEmpty()) {
+      headers.add(new MimeVersionHeader(1.0));
+      headers.add(new ContentTypeHeader("multipart/alternative", null, "frontier"));
+      parts.add(new MessagePart(headers, "This is a message with multiple parts in MIME format."));
+      headers.clear();
+      if (ASCIICharSequence.isAscii(text)) {
+        headers.add(new ContentTypeHeader("text/plain", "us-ascii", "frontier"));
+      } else {
+        headers.add(new ContentTypeHeader("text/plain", "utf-8", "frontier"));
+        headers.add(new ContentTransferEncodingHeader("base64"));
+      }
+      parts.add(new MessagePart(headers, text));
+      headers.clear();
+      headers.add(new ContentTypeHeader("text/html", "utf-8", "frontier"));
+      headers.add(new ContentTransferEncodingHeader("base64"));
+      parts.add(new MessagePart(headers, html));
+    } else if (!text.isEmpty()) {
+      if (ASCIICharSequence.isAscii(text)) {
+        headers.add(new ContentTypeHeader("text/plain", "us-ascii", null));
+        parts.add(new MessagePart(headers, text));
+      } else {
+        headers.add(new ContentTypeHeader("text/plain", "utf-8", null));
+        headers.add(new ContentTransferEncodingHeader("base64"));
+        parts.add(new MessagePart(headers, text));
+      }
+    } else if (!html.isEmpty()) {
+      headers.add(new ContentTypeHeader("text/html", null, null));
+      headers.add(new ContentTransferEncodingHeader("base64"));
+      parts.add(new MessagePart(headers, html));
+    }
+
+    System.out.println(new Message(parts, true).encodeMessage());
+  }
 
 }
