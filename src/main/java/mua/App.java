3642843112edc9dd3092962f38b3cc8160e6e4cd
Davide Garberi <dade.garberi@gmail.com>
2024-02-07 21:19:52 +0100

    src: main: mua: Add MailboxManager and initial App LSM

--- src/main/java/mua/App.java
+++ src/main/java/mua/App.java
@@ -1,7 +1,20 @@
 package mua;
 
+import utils.ASCIICharSequence;
+import utils.EntryEncoding;
+import utils.Fragment;
+import utils.Storage;
+import utils.UIInteract;
+
+import java.util.List;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 /** The application class */
 public class App {
+  static private MailboxManager mailboxManager;
 
   /**
    * Runs the REPL.
@@ -9,6 +22,48 @@ public class App {
    * <p>Develop here the REPL, see the README.md for more details.
    *
    * @param args the first argument is the mailbox base directory.
+   * @throws IOException
    */
-  public static void main(String[] args) {}
+  public static void main(String[] args) throws IOException {
+    if (args.length <= 0)
+      return;
+
+    String mailboxBaseDir = args[0];
+    Storage storage = new Storage(mailboxBaseDir);
+    mailboxManager = new MailboxManager(storage);
+
+    for (Storage.Box box : storage.boxes()) {
+      System.out.println(box);
+    }
+
+    try (UIInteract ui = UIInteract.getInstance()) {
+      for (;;) {
+        String[] input = ui.command("> ");
+        if (input == null) break;
+        switch (input[0]) {
+          case "LSM":
+            ui.output("You requested an lsm...");
+            printMailboxes(new ArrayList<>(mailboxManager.getMailboxMap().keySet()));
+            break;
+          case "CD":
+            ui.output("You requested a cd...");
+            break;
+          default:
+            ui.error("Unknown command: " + input[0]);
+            break;
+        }
+      }
+    }
+  }
+
+  /**
+   * Prints the list of mailboxes to the standard output.
+   *
+   * @param mailboxes the list of mailboxes
+   */
+  public static void printMailboxes(List<Mailbox> mailboxes) {
+    for (Mailbox mailbox : mailboxes) {
+      System.out.println(mailboxManager.getMailboxMap().get(mailbox));
+    }
+  }
 }
---
+++ src/main/java/mua/MailboxManager.java
@@ -0,0 +1,84 @@
+package mua;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Collections;
+
+import utils.*;
+
+/**
+ * The MailboxManager class represents a manager for mailboxes.
+ * It provides methods to add and remove mailboxes.
+ */
+public class MailboxManager {
+    private final Map<Mailbox, Storage.Box> mailboxMap;
+    private final Map<Message, Storage.Box.Entry> messageMap;
+
+    /**
+     * Constructs a new MailboxManager object with the given list of mailboxes.
+     *
+     * @param mailboxes the list of mailboxes
+     */
+    public MailboxManager(Storage storage) {
+        mailboxMap = new LinkedHashMap<>();
+        messageMap = new LinkedHashMap<>();
+
+        for (Storage.Box storageBox : storage.boxes()) {
+            List<Message> messages = new ArrayList<>();
+            for (Storage.Box.Entry entry : storageBox.entries()) {
+                ASCIICharSequence sequence = entry.content();
+                List<Fragment> fragments = EntryEncoding.decode(sequence);
+                Message message = new Message(Message.createMessageParts(fragments));
+                messages.add(message);
+                messageMap.put(message, entry);
+            }
+            Mailbox mailbox = new Mailbox(messages);
+            mailboxMap.put(mailbox, storageBox);
+        }
+    }
+
+    /**
+     * Adds a mailbox to the manager.
+     * @param mailbox The mailbox to be added.
+     */
+    /*public void addMailbox(Mailbox mailbox) {
+        mailboxes.add(mailbox);
+    }*/
+
+    /**
+     * Removes a mailbox from the manager.
+     * @param mailbox The mailbox to be removed.
+     */
+    /*public void removeMailbox(Mailbox mailbox) {
+        mailboxes.remove(mailbox);
+    }*/
+
+    /**
+     * Returns the list of mailboxes.
+     * @return the list of mailboxes
+     */
+    /*public List<Mailbox> getMailboxes() {
+        return Collections.unmodifiableList(mailboxes);
+    }*/
+
+    /**
+     * Returns the Map of mailboxes and their corresponding Storage.Box.
+     *
+     * @return the Map of mailboxes and their corresponding Storage.Box
+     */
+    public Map<Mailbox, Storage.Box> getMailboxMap() {
+        return Collections.unmodifiableMap(mailboxMap);
+    }
+
+    /**
+     * Returns the Map of messages and their corresponding Storage.Box.Entry.
+     *
+     * @return the Map of messages and their corresponding Storage.Box.Entry
+     */
+    public Map<Message, Storage.Box.Entry> getMessageMap() {
+        return Collections.unmodifiableMap(messageMap);
+    }
+}
