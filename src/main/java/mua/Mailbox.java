78d7f999adfb17033eb913be1a11736adc248829
Davide Garberi <dade.garberi@gmail.com>
2024-02-11 19:27:57 +0100

    Mailbox: AF, RI and safety checks

--- src/main/java/mua/Mailbox.java
+++ src/main/java/mua/Mailbox.java
@@ -8,6 +8,21 @@ import java.util.List;
 
 /** Represents a mailbox that stores messages. */
 public final class Mailbox {
+  /*
+   * Abstraction Function:
+   * Represents a mailbox that stores messages.
+   * An instance a of Mailbox represents a mailbox with the name a.name and the list of messages a.messages.
+   * The list of messages is sorted by date in descending order, when returned by getMessages(), using
+   * the date header of the messages.
+   * The mailbox can be modified by adding or removing messages.
+   *
+   * Representation Invariant:
+   * - messages is not null
+   * - messages is not empty
+   * - name is not null
+   * - name is not empty
+   * - all messages in messages are not null, this is enforced by the constructor of Message
+   */
   /** The name of the mailbox */
   public final String name;
 
@@ -15,18 +30,25 @@ public final class Mailbox {
   private final List<Message> messages;
 
   /**
-   * Constructs a Mailbox object with the given list of messages.
+   * Constructs a Mailbox object with the given list of messages and name.
    *
    * @param messages the list of messages to be stored in the mailbox
    * @param name the name of the mailbox
+   * @throws IllegalArgumentException if the list of messages is null or empty
+   * @throws IllegalArgumentException if the name is null or empty
    */
-  public Mailbox(List<Message> messages, String name) {
-    this.messages = messages;
+  public Mailbox(List<Message> messages, String name) throws IllegalArgumentException {
+    if (messages == null) throw new IllegalArgumentException("Messages cannot be null");
+    if (messages.isEmpty()) throw new IllegalArgumentException("Messages cannot be empty");
+    if (name == null) throw new IllegalArgumentException("Name cannot be null");
+    if (name.isBlank()) throw new IllegalArgumentException("Name cannot be empty");
+
+    this.messages = new ArrayList<>(messages);
     this.name = name;
   }
 
   /**
-   * Returns the list of messages sorted by date in descending order.
+   * Returns a copy of the list of messages, sorted by date in descending order.
    *
    * @return the sorted list of messages
    */
@@ -63,14 +85,5 @@ public final class Mailbox {
    */
   public void removeMessage(Message message) {
     messages.remove(message);
-  }
-
-  /**
-   * Removes a message at the specified index from the mailbox.
-   *
-   * @param index the index of the message to be removed
-   */
-  public void removeMessage(int index) {
-    messages.remove(index);
   }
 }
