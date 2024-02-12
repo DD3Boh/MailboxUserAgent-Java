b0a9872b9e0ddb08c176f65f84246ee51a489d1f
Davide Garberi <dade.garberi@gmail.com>
2024-02-12 03:46:13 +0100

    SubjectHeader: Update documentation and safety checks

--- src/main/java/mua/SubjectHeader.java
+++ src/main/java/mua/SubjectHeader.java
@@ -5,19 +5,34 @@ import utils.Base64Encoding;
 
 /** Represents the subject header of a message. */
 public final class SubjectHeader implements Header<String> {
+  /*
+   * Abstraction Function:
+   * Represents a Subject header, with value being the subject of the message.
+   * The value is represented as a String.
+   * If the value is encoded in Base64, it is decoded to a String before being stored.
+   * If the value is not encoded in Base64, it is stored as is.
+   * When the header is encoded to ASCII, the value is encoded in Base64 if it contains non-ASCII characters,
+   * and stored as is if it contains only ASCII characters.
+   *
+   * Representation Invariant:
+   * - value is not null or empty.
+   */
   /** The type of the header */
   public static final String TYPE = "Subject";
   /** The value of the Subject header */
-  private String value;
+  private final String value;
 
   /**
-   * Constructs a SubjectHeader object with the specified String. The value is decoded if it is
-   * encoded in Base64.
+   * Constructs a SubjectHeader object with the specified String.
+   * The value is decoded if it is encoded in Base64.
    *
    * @param value the subject of the message as a String.
+   * @throws IllegalArgumentException if the value is null or empty
    */
   public SubjectHeader(String value) {
     String subject;
+    if (value == null || value.isEmpty()) throw new IllegalArgumentException("The value cannot be null or empty");
+
     if (ASCIICharSequence.isAscii(value)) {
       ASCIICharSequence ascii = ASCIICharSequence.of(value);
 
@@ -51,6 +66,7 @@ public final class SubjectHeader implements Header<String> {
 
   /**
    * Returns an ASCII representation of the SubjectHeader object.
+   * The ASCII representation is of the form "Subject: value".
    *
    * @return an ASCII representation of the SubjectHeader object
    */
