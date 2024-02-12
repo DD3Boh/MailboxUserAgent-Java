d413984c401456c4dd978e2502cbb4479d5b4f06
Davide Garberi <dade.garberi@gmail.com>
2024-02-12 11:49:59 +0100

    src: MessagePart: Automatic reformatting

--- src/main/java/mua/MessagePart.java
+++ src/main/java/mua/MessagePart.java
@@ -187,11 +187,10 @@ public final class MessagePart {
   }
 
   /**
-   * Returns the decoded body of the message part. 
-   * If the body is not encoded, the original body is returned.
+   * Returns the decoded body of the message part. If the body is not encoded, the original body is
+   * returned.
    *
-   * The ContentTransferEncodingHeader is used to check if
-   * the body is decoded or not.
+   * <p>The ContentTransferEncodingHeader is used to check if the body is decoded or not.
    *
    * @return the decoded body of the message part
    */
