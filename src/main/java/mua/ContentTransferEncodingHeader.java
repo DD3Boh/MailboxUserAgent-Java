8f9f44e683b2c351bd5ce0c22aa65a6804c71369
Davide Garberi <dade.garberi@gmail.com>
2024-02-12 02:55:37 +0100

    ContentTransferEncodingHeader: Re-write documentation and misc

--- src/main/java/mua/ContentTransferEncodingHeader.java
+++ src/main/java/mua/ContentTransferEncodingHeader.java
@@ -4,6 +4,14 @@ import utils.ASCIICharSequence;
 
 /** Represents a Content-Transfer-Encoding header. */
 public final class ContentTransferEncodingHeader implements Header<String> {
+  /*
+   * Abstraction Function:
+   * Represents a Content-Transfer-Encoding header with a value.
+   * The Content-Transfer-Encoding header is used to specify the encoding type of the content.
+   *
+   * Representation Invariant:
+   * - The value cannot be null or empty.
+   */
   /** the value of the Content-Transfer-Encoding header */
   private final String value;
 
@@ -11,8 +19,11 @@ public final class ContentTransferEncodingHeader implements Header<String> {
    * Constructs a new Content-Transfer-Encoding header with the specified encoding.
    *
    * @param value the encoding type
+   * @throws IllegalArgumentException if the value is null or empty
    */
-  public ContentTransferEncodingHeader(String value) {
+  public ContentTransferEncodingHeader(String value) throws IllegalArgumentException {
+    if (value == null || value.isEmpty()) throw new IllegalArgumentException("The value cannot be null or empty");
+
     this.value = value;
   }
 
@@ -36,18 +47,14 @@ public final class ContentTransferEncodingHeader implements Header<String> {
     return value;
   }
 
+  /**
+   * Returns an ASCII representation of the Content-Transfer-Encoding header.
+   * The ASCII representation is of the form "Content-Transfer-Encoding: value".
+   *
+   * @return an ASCII representation of the Content-Transfer-Encoding header
+   */
   @Override
   public ASCIICharSequence encodeToASCII() {
     return ASCIICharSequence.of(getType() + ": " + getValue());
-  }
-
-  /**
-   * Returns a string representation of the Content-Transfer-Encoding header.
-   *
-   * @return a string representation of the Content-Transfer-Encoding header
-   */
-  @Override
-  public String toString() {
-    return value;
   }
 }
